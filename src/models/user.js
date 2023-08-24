ffca2eccf3a38b7df719b41dbde6d85378fc9685
ghulammohiodin <ghulammohiodin321@gmail.com>
2023-08-24 20:43:06 +0500

    cpus

--- src/models/user.js
+++ src/models/user.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
     interests: [],
     locations: [],
     diseases: [],
+    image: {
+      type: String,
+      default: "",
+    },
   },
   {
     timestamps: true,
